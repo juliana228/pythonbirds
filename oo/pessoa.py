6c1583e26fd24c0b7f5f79de95a62f06313ac73c
juliana228 <julianamedeiros_228@hotmail.com>
2021-11-30 23:51:42 -0300

    Criada a classe carro

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,3 +1,19 @@
-class Pessoa:
-    def comprimentar(self):
-        p = Pessoa
+class Carro:
+    def __init__(self, marca, preco, cor):
+        self.marca = marca
+        self.cor = cor
+        self.preco = preco
+
+    def ligar(self):
+        print('Ligando o carro')
+
+    def desligar(self):
+        print('desligando o carro')
+
+    def ExibirInformacoes(self):
+        print(f'O carro é da marca {self.marca}, custa R$ {self.preco} e possui cor {self.cor}')
+
+carro1 = Carro('Honda',150000,'vermelha')
+carro1.ligar()
+carro1.desligar()
+carro1.ExibirInformacoes()
